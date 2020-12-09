5a164e9cc5b20da3cf5293f0adf5cca13ff5c1a8
최현우 <escape0619@gmail.com>
2020-12-09 09:29:49 +0900

    message commit

--- src/main/java/com/spring/security/controller/user/MessageController.java
+++ src/main/java/com/spring/security/controller/user/MessageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MessageController {
 
-    @GetMapping("/message")
+    @GetMapping("/messages")
     public String message(){
 
         return "user/messages";
