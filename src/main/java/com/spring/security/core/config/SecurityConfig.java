6f8d6eaf9d92a682e68965baac77b1eb2960a195
최현우 <escape0619@gmail.com>
2020-12-10 09:09:50 +0900

    add) web ignore static resource(ex] .css, .js, .html ... etc) 경로

--- src/main/java/com/spring/security/core/config/SecurityConfig.java
+++ src/main/java/com/spring/security/core/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.spring.security.core.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -19,13 +21,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         String password = passwordEncoder().encode("1111");
 
         auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
-        auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER");
-        auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER", "USER");
+        auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN", "MANAGER", "USER");
 
     }
     // password encoder get
     private PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        // uri 권한 예외 설정 static resource(ex] .css, .js, .html ... etc) 경로
+        // permitAll()과 비슷 -> 차이는 permitAll()은 filter 내부로 들어옴, ignore는 security filter를 거치지 않음.
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
     @Override
